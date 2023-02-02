dbf89f967cf3d126ca4ea5caeabd70ca1870b847
Utkarsh Hadgekar <79980596+utkarsh-1602@users.noreply.github.com>
2023-02-02 11:46:49 +0530

    sol balance-fetcher

---
+++ solana-intro-frontend/pages/index.tsx
@@ -0,0 +1,52 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import AddressForm from '../components/AddressForm'
+import * as web3 from "@solana/web3.js"
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState(0)
+  const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false); 
+
+  const addressSubmittedHandler = (address: string) => {
+    try{
+      setAddress(address)
+      const key = new web3.PublicKey(address)
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
+      })
+      
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false); 
+      })
+    }
+    catch (error){
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p className={styles.Title}>
+          SOL Balance Fetcher 
+        </p>
+        <AddressForm handler={addressSubmittedHandler} />
+        <p>{`Address: ${address}`}</p>
+        <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it Executable: ${isExecutable ? 'Yep' : 'Nope'}`}</p>
+        <div className={styles.footer}>Solana#proj1: twitter - ft_utkarsh</div>
+      </header>
+      
+    </div>
+  
+  )
+}
+
+export default Home
